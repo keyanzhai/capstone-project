223fcaa64ecfd587eddf58500974630d21974e10
Keyan Zhai <keyanzhai@ist-06175.apn.wlan.private.upenn.edu>
2023-03-31 16:53:31 -0400

    add rom to pose-recorded and save annotated image

--- pose-recorded.py
+++ pose-recorded.py
@@ -22,13 +22,26 @@ with mp_pose.Pose(
     # Convert the BGR image to RGB before processing.
     results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    if not results.pose_landmarks:
-      continue
-    print(
-        f'Nose coordinates: ('
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-    )
+    # if not results.pose_landmarks:
+    #   continue
+    # print(
+    #     f'Nose coordinates: ('
+    #     f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+    #     f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+    # )
+
+    if 'landmark' in dir(results.pose_world_landmarks):
+        right_wrist = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
+        right_wrist_pos = np.array([right_wrist.x, right_wrist.y, right_wrist.z])
+
+        right_elbow = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW]
+        right_elbow_pos = np.array([right_elbow.x, right_elbow.y, right_elbow.z])
+
+        right_shoulder = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        right_shoulder_pos = np.array([right_shoulder.x, right_shoulder.y, right_shoulder.z])
+
+        rom_right_elbow = rom.rom_right_elbow(right_wrist_pos, right_elbow_pos, right_shoulder_pos)
+        print(rom_right_elbow)
 
     annotated_image = image.copy()
     # Draw segmentation on the image.
@@ -44,7 +57,7 @@ with mp_pose.Pose(
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+    cv2.imwrite('tmp/annotated_image' + str(idx) + '.png', annotated_image)
     # Plot pose world landmarks.
     mp_drawing.plot_landmarks(
         results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
