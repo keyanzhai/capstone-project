7084f86951a86276a332d48a06a6a60f7128f1b3
Keyan Zhai <keyanzhai@ist-06175.apn.wlan.private.upenn.edu>
2023-03-31 16:28:21 -0400

    separate webcam and recorded image

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # MediaPipe Pose
 
+## Demo
+
+```
+source mp_env/bin/activate
+python3 pose-recorded.py
+python3 pose-webcam.py
+```
+
 ## Output Format
 
 <https://google.github.io/mediapipe/solutions/pose#pose_landmarks>
@@ -12,13 +20,8 @@
 * pose_world_landmarks
 * segmentation_mask
 
----
-
-No orientation / joint angle?
-
----
+All joints:
 
 ```
-source mp_env/bin/activate
-python3 pose.py
+['LEFT_ANKLE', 'LEFT_EAR', 'LEFT_ELBOW', 'LEFT_EYE', 'LEFT_EYE_INNER', 'LEFT_EYE_OUTER', 'LEFT_FOOT_INDEX', 'LEFT_HEEL', 'LEFT_HIP', 'LEFT_INDEX', 'LEFT_KNEE', 'LEFT_PINKY', 'LEFT_SHOULDER', 'LEFT_THUMB', 'LEFT_WRIST', 'MOUTH_LEFT', 'MOUTH_RIGHT', 'NOSE', 'RIGHT_ANKLE', 'RIGHT_EAR', 'RIGHT_ELBOW', 'RIGHT_EYE', 'RIGHT_EYE_INNER', 'RIGHT_EYE_OUTER', 'RIGHT_FOOT_INDEX', 'RIGHT_HEEL', 'RIGHT_HIP', 'RIGHT_INDEX', 'RIGHT_KNEE', 'RIGHT_PINKY', 'RIGHT_SHOULDER', 'RIGHT_THUMB', 'RIGHT_WRIST']
 ```
---
+++ pose-webcam.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import rom
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
+
+    if 'landmark' in dir(results.pose_world_landmarks):
+        right_wrist = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
+        # right_wrist_pos = np.array([right_wrist.x, right_wrist.y, right_wrist.z])
+        right_wrist_pos = np.array([right_wrist.x, right_wrist.y])
+
+        right_elbow = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW]
+        # right_elbow_pos = np.array([right_elbow.x, right_elbow.y, right_elbow.z])
+        right_elbow_pos = np.array([right_elbow.x, right_elbow.y])
+
+        right_shoulder = results.pose_world_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        # right_shoulder_pos = np.array([right_shoulder.x, right_shoulder.y, right_shoulder.z])
+        right_shoulder_pos = np.array([right_shoulder.x, right_shoulder.y])
+
+        rom_right_elbow = rom.rom_right_elbow(right_wrist_pos, right_elbow_pos, right_shoulder_pos)
+        print(rom_right_elbow)
+
+        
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
